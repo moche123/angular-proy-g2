92084781f8f3fae2d541d17de4cf5ff16c0195da
moche123 <mochemiguel@gmail.com>
2022-09-16 22:33:18 -0500

    changes fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://rickandmortyapi.com/api/character',
-  baseUrl: 'http://localhost:4000/api'
+  baseUrl: 'https://ceti-angular-g2.herokuapp.com/api'
 };
